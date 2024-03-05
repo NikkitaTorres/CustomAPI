23d2ab8af0eece9c4c8d3b058aad615cbbdcf9ff
NikkitaTorres <nikkitatorres@yahoo.com>
2024-03-05 09:29:52 -0800

    update Models/Shoe

---
+++ ShoeStoreApi/Models/Shoe.cs
@@ -0,0 +1,10 @@
+namespace ShoeStoreApi.Models
+{
+  public class Shoe
+  {
+    public int ShoeId { get; set; }
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public string Colors { get; set; }
+  }
+}
